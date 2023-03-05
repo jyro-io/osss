625a589a9c2ff301d14eef8d3aef517a05d243e3
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 11:56:16 -0600

    updates to build scripts

--- camera/build.sh
+++ camera/build.sh
@@ -2,4 +2,14 @@
 
 # this script builds the camera application and installs it to an RPi OS image.
 
-wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2023-02-22/2023-02-21-raspios-bullseye-armhf-lite.img.xz
+if [ ! -f "2023-02-21-raspios-bullseye-armhf-lite.img.xz" ]; then
+  wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2023-02-22/2023-02-21-raspios-bullseye-armhf-lite.img.xz
+fi
+
+if [ ! -f "2023-02-21-raspios-bullseye-armhf-lite.img" ]; then
+  xz --decompress --keep 2023-02-21-raspios-bullseye-armhf-lite.img.xz
+fi
+
+mount -o loop 2023-02-21-raspios-bullseye-armhf-lite.img img/
+
+umount img/
--- monitor/build.sh
+++ monitor/build.sh
@@ -2,4 +2,14 @@
 
 # this script builds the monitor application and installs it to an RPi OS image.
 
-wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-02-22/2023-02-21-raspios-bullseye-arm64.img.xz
+if [ ! -f "2023-02-21-raspios-bullseye-arm64.img.xz" ]; then
+  wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-02-22/2023-02-21-raspios-bullseye-arm64.img.xz
+fi
+
+if [ ! -f "2023-02-21-raspios-bullseye-arm64.img" ]; then
+  xz --decompress --keep 2023-02-21-raspios-bullseye-arm64.img.xz
+fi
+
+mount -o loop 2023-02-21-raspios-bullseye-arm64.img img/
+
+umount img/
