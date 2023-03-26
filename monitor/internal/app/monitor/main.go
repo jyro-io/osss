38134bf71ac9fb6f464a4f6d56883e8c27b4a3ce
Rory Linehan <rorylinehan@protonmail.com>
2023-03-26 16:14:27 -0500

    incremental update

--- monitor/internal/app/monitor/main.go
+++ monitor/internal/app/monitor/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
 
 	"github.com/davecgh/go-spew/spew"
 	"gopkg.in/yaml.v2"
@@ -13,8 +15,14 @@ func logError(error error) {
 	log.Println("osss: monitor: error:", error)
 }
 
+func logLine(message string) {
+	log.Println(fmt.Sprintf("osss: monitor: %s", message))
+}
+
 type Config struct {
 	Monitor struct {
+		cameraPort  int
+		monitorPort int
 	}
 }
 
@@ -32,26 +40,28 @@ func getConfig() Config {
 }
 
 func main() {
-	log.Println("osss: monitor: info: started")
+	logLine("started")
 	config := getConfig()
-	log.Println("osss: monitor: info: loaded config.yaml:")
+	logLine("loaded config.yaml")
 	spew.Dump(config)
 
 	// start camera stream listening server
-	cameraListener, err := net.Listen("tcp", "0.0.0.0:7776")
+	cameraEndpoint := fmt.Sprintf("0.0.0.0:%s", strconv.Itoa(config.Monitor.cameraPort))
+	cameraListener, err := net.Listen("tcp", cameraEndpoint)
 	if err != nil {
 		logError(err)
 		return
 	}
-	log.Println("osss: monitor: started camera listener on 0.0.0.0:7776")
+	logLine(fmt.Sprintf("started camera listener on %s", cameraEndpoint))
 
 	// start localhost camera stream monitoring server
-	monitorListener, err := net.Listen("tcp", "0.0.0.0:7777")
+	monitorEndpoint := fmt.Sprintf("0.0.0.0:%s", strconv.Itoa(config.Monitor.cameraPort))
+	monitorListener, err := net.Listen("tcp", monitorEndpoint)
 	if err != nil {
 		logError(err)
 		return
 	}
-	log.Println("osss: monitor: started camera listener on 0.0.0.0:7777")
+	logLine(fmt.Sprintf("started monitor listener on %s", monitorEndpoint))
 
 	for {
 		conn, err := cameraListener.Accept()
@@ -59,13 +69,13 @@ func main() {
 			logError(err)
 			return
 		}
-		log.Printf("osss: monitor: accepted camera connection from: %v\n", conn.RemoteAddr())
+		logLine(fmt.Sprintf("accepted camera connection from: %s", conn.RemoteAddr()))
 		conn, err = monitorListener.Accept()
 		if err != nil {
 			logError(err)
 			return
 		}
-		log.Printf("osss: monitor: accepted monitor connection from: %v\n", conn.RemoteAddr())
+		logLine(fmt.Sprintf("accepted monitor connection from: %s", conn.RemoteAddr()))
 		// watch camera streams for data
 		// switch localhost:7777 stream to most recently active camera
 	}
